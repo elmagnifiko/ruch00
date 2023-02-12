414f43225dd616c1afdde4f0fe1cceefa3871a69
elmagnifiko <121030644+elmagnifiko@users.noreply.github.com>
2023-02-12 22:53:43 +0100

    Add files via upload

--- numberToString.js
+++ numberToString.js
@@ -1,12 +1,12 @@
-const numbers =  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const numbers =  [2, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const words =  ["un", "deux", "trois", "quatre", "cinq", "six", "sept", "huit",
 "neuf", "dix"];
 
-const numberToWords = (numbers) => {
+const numberToString = (numbers) => {
     return numbers.map((numbers) => {
       return words[numbers - 1];
     });
   };
-  console.log(numberToWords(numbers));
+  console.log(numberToString(numbers));
 
-  module.exports = numberToWords(numbers);
+  module.exports = numberToString(numbers);
