138ce4b142832f4be4919079b19ed383e9e41f33
elmagnifiko <121030644+elmagnifiko@users.noreply.github.com>
2023-02-13 11:55:50 +0100

    Add files via upload

--- numberToString.js
+++ numberToString.js
@@ -1,4 +1,4 @@
-const numbers =  [2, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const numbers =  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const words =  ["un", "deux", "trois", "quatre", "cinq", "six", "sept", "huit",
 "neuf", "dix"];
 
